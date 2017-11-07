ba4907c665981215efa5a5f8a271f9c3986f0ce4
DSchroederOSU <schrodan@oregonstate.edu>
2017-11-07 15:30:18 -0800

    js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-var express = require('express')
-var ws = require('./ws')
+var express = require('express');
+var ws = require('./ws');
 
-var app = express()
+var app = express();
 
 app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/ws.html');
+    res.sendFile(__dirname + '/ws.html');
 })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
--- ws.js
+++ ws.js
@@ -4,7 +4,7 @@ var WebSocketServer = require('ws').Server,
 wss.on('connection', function (ws) {
   ws.on('message', function (message) {
     console.log('received: %s', message)
-  })
+  }),
 
   setInterval(
     () => ws.send(`${new Date()}`),
