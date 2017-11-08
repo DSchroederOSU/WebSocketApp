7d1d6f53110497fc70964b6cfd15c8002857ece5
DSchroederOSU <schrodan@oregonstate.edu>
2017-11-08 00:39:04 -0800

    added some d3 and formatted time differently

--- public/js/main.js
+++ public/js/main.js
@@ -13,15 +13,14 @@ app.controller('myController', function ($scope, websocketService) {
         var json = JSON.parse(obj);
 
         $scope.$apply(function () {
-            $scope.hours = json.hours;
-            $scope.seconds = json.seconds;
-            $scope.minutes = json.minutes;
-            $scope.time = obj
+
             if (json.am == 0)
-                $scope.am = "AM"
+                $scope.time = json.hours + " : " + json.minutes + " : " +  json.seconds + " AM";
             else
-                $scope.am = "PM"
+                $scope.time = json.hours + " : " + json.minutes + " : " +  json.seconds + " PM";
+
         });
+
     });
 });
 
@@ -49,6 +48,14 @@ function formatTime(time){
         hours = hours - 12;
         ampm = 1;
     }
+    else if(hours == 0){
+        hours = 12;
+        ampm = 0;
+    }
+    if (hours.toString().length == 1)
+        hours = "0"+hours;
+
+
     var sec = parseInt(time.substring(18,20));
     if (sec.toString().length == 1)
         sec = "0"+sec;
@@ -56,3 +63,20 @@ function formatTime(time){
     return JSON.stringify({ seconds: sec, hours: hours,
         minutes: parseInt(time.substring(15,17)), am: ampm});
 }
+
+http://bl.ocks.org/biovisualize/5372077
+$( document ).ready(function() {
+    var barChart = d3.select("#chart")
+        .append("svg")
+        .chart("BarChart")
+        .width(200)
+        .height(150);
+
+    barChart.draw([
+        {name: 'a', value: 2},
+        {name: 'b', value: 16},
+        {name: 'c', value: 19},
+        {name: 'd', value: 8},
+        {name: 'e', value: 6}
+    ]);
+});
